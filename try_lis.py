8acf0e0181a8eefd870d111e80f0c89e1c7e50a0
Mitsuki1025 <menka123nachi16@icloud.com>
2023-09-09 18:46:57 +0900

    improve error Have you called init_node()?

---
+++ try_lis.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+
+##################################################
+
+class TurtleListener():
+    def __init__(self): 
+            self.subscription = self.create_subscription(Twist,'cmd_vel', 10) 
+            rospy.init_node('turtle_try_tal_node') 
+    
+def main(args=None):
+    rospy.init(args=args) # rospyモジュールの初期化
+    
+if __name__ == '__main__':  # 初期化宣言  最初に実行
+     rospy.init_node("task_test_lis_node") 
+     rospy.spin() 
+     cmd_vel = Twist()
+                        
