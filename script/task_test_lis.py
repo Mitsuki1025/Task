3bb7ea0d7b94bb0ddf8706bc33c4f316710989a3
Mitsuki1025 <141213109+Mitsuki1025@users.noreply.github.com>
2023-09-11 05:43:06 +0900

    Update task_test_lis.py

--- script/task_test_lis.py
+++ script/task_test_lis.py
@@ -5,31 +5,16 @@ from geometry_msgs.msg import Twist
 
 ##################################################
 
-class TurtleListener():
-    def __init__(self): #コンストラクタ　__init__
-                        #selfはインスタンス変数？
-            super().__init__('turtle_listener_node')
-            self.subscription = self.create_subscription(Twist,'cmd_vel', self.listener_callback, 10) 
-#この辺違うかも　参照：https://wiki-ros-org.translate.goog/turtlesim/Tutorials/Go%20to%20Goal?_x_tr_sch=http&_x_tr_sl=en&_x_tr_tl=ja&_x_tr_hl=ja&_x_tr_pto=sc
-            #subscriberの生成
-            #1番目の引数　Twist＝メッセージ型
-            #2番目の引数　cmd_vel トピック名　？
-            #3番目の引数  callback関数
-            #4番目の引数  キューのサイズ　？
-            
-    # コールバック定義は，受信したデータとともに，情報メッセージをコンソールに出力するだけ．        
-    def listener_callback(self, Twist):
-            #2番目の引数　Twist＝メッセージ型
-            self.get_logger().info("Velocity: Linear=%f angular=%f" % (Twist.linear.x,Twist.angular.z)) 
+class TurtleTryLis():
+    def __init__(self): 
+            self.subscription = self.create_subscription(Twist,'cmd_vel', 10) 
+            rospy.init_node('turtle_try_tal_node')
 
     
 def main(args=None):
     rospy.init(args=args) # rospyモジュールの初期化
     
-    
 if __name__ == '__main__':  # 初期化宣言  最初に実行
-     rospy.init_node("task_test_lis_node") 
-     turtle_listener = TurtleListener()   #ノードの作成
-     rospy.spin() 
-     cmd_vel = Twist()
-                          #ノードの破壊したほうがいい？　参照：https://qiita.com/ItoMasaki/items/6acabc94b040f8b0f7cd
+    cmd_vel = Twist() 
+    rospy.init_node("task_test_lis_node") 
+    rospy.spin() 
