53d3a912326433aab96553c27f24ce314900fc6c
Mitsuki1025 <menka123nachi16@icloud.com>
2023-09-13 02:17:44 +0900

    rm

--- task_test_tal.py
+++ task_test_tal.py
@@ -4,6 +4,7 @@ import rospy
 from geometry_msgs.msg import Twist
 
 
+
 ###########################################################
 
 class Talker():  
@@ -11,26 +12,10 @@ class Talker():
         self.twist_pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
         
     def publish(self):
-            cmd_vel = Twist()
-
-            cmd_vel.linear.x  =  0
-            cmd_vel.angular.z =  0 
-
-            cmd_vel.linear.x  =  0.5
-            cmd_vel.angular.z =  0 
-
-
-            cmd_vel.linear.x  = -0.5
-            cmd_vel.angular.z =  0 
-            
-            cmd_vel.linear.x  =  0
-            cmd_vel.angular.z =  1.0
-            
-            cmd_vel.linear.x  =  0
-            cmd_vel.angular.z = -1.0
-        
+            cmd_vel = Twist()            
+            cmd_vel.linear.x = 2.0
+            cmd_vel.angular.z = 0.0
             self.twist_pub.publish(cmd_vel)
-      
 
                              # rospyモジュールの初期化    
 if __name__ == '__main__':   #①初期化宣言  最初に実行
