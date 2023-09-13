257b4a8119e7194e49ad42d872e5adcb9111ae34
Mitsuki1025 <menka123nachi16@icloud.com>
2023-09-13 20:40:30 +0900

    improve task

--- task_test_tal.py
+++ task_test_tal.py
@@ -2,6 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+import datetime
 
 
 
@@ -10,13 +11,25 @@ from geometry_msgs.msg import Twist
 class Talker():  
     def __init__(self):
         self.twist_pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
-        
-    def publish(self):
-            cmd_vel = Twist()            
-            cmd_vel.linear.x = 2.0
-            cmd_vel.angular.z = 0.0
-            self.twist_pub.publish(cmd_vel)
 
+    def publish(self):
+            cmd_vel = Twist() 
+            
+            dt = datetime.datetime.today()  # ローカルな現在の日付と時刻を取得 
+            print(f'hour: {dt.hour}, minute: {dt.minute}, second: {dt.second}')  
+           
+      
+            if  (int(dt.hour) <=  11 ): #１時〜１１時
+                  cmd_vel.linear.z = -0.3
+            elif ( int(dt.hour) ==  12 ): #12時 
+                  cmd_vel.linear.y = -0.3
+            elif (int(dt.hour) >=  23 ): #１３時から２３時まで
+                  cmd_vel.linear.z = 0.3
+            else:                   
+                  cmd_vel.linear.y = 0.3 #24時 
+
+            self.twist_pub.publish(cmd_vel)
+            
                              # rospyモジュールの初期化    
 if __name__ == '__main__':   #①初期化宣言  最初に実行
      rospy.init_node("task_test_tal_node", anonymous=True) 
